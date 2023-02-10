7dcc97e8269c50e1c586167aa72e2d728c7844b0
Nowee <nowee018@naver.com>
2023-02-10 14:26:32 +0900

    inferface , 함수 추상화 완성

--- Todo.ts
+++ Todo.ts
@@ -1,7 +1,30 @@
-interface Todo{
+interface ITodo{
     id:number;
     content:string;
     isCompleted:string;
     category:string;
-    tag?:string[];
+    tag?:string[]; // 선택적으로 구현하는 프로퍼티 (있어도 되고 없어도 됨)
 }
+// 인터페이스는 클래스와 개념이 유사하지만 객체를 생성할 수 없으므로 주로 타입 검사를 위해서 활용 
+
+interface ICreateTodo{
+   (todo: ITodo);
+}
+
+interface IGetTodo{
+    (id: number): ITodo;
+}
+
+interface IUpdateTodo{
+    (ITodo: IUpdateTodo);
+}
+
+interface IDeleteTodo{
+    (id:number);
+}
+
+const CreateTodo:ICreateTodo = (todo) =>{};
+
+const UpdateTodo:IUpdateTodo = (todo)=>{};
+
+const DeleteId:IDeleteTodo = (id) => {};
